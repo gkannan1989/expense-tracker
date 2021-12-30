053dee67e29eeb399b207c0f3dad6fd5b1604faa
Kannan Ganesan <kannan.ganesan@simplybusiness.co.uk>
2021-12-30 20:26:22 +0000

    rendering main

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense, useRef } from 'react';
 import { Grid } from '@material-ui/core';
+import { Main } from './components'
 import useStyles from './styles';
 import ImageList from '@material-ui/core/ImageList';
 import ImageListItem from '@material-ui/core/ImageListItem';
@@ -9,7 +10,6 @@ const App = () => {
   const classes = useStyles();
   const main = useRef(null)
   const Details = React.lazy(() => import('./components/Details/Details'));
-  const Main = React.lazy(() => import('./components/Main/Main'));
 
   return (
     <div>
@@ -25,9 +25,7 @@ const App = () => {
      </ImageList> 
       <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '100vh'}}>
         <Grid ref={main} item xs={12} sm={4} className={classes.main}>
-         <Suspense fallback={<>Loading</>}>
           <Main />
-         </Suspense>
         </Grid>
         <Grid item xs={12} sm={4} className={classes.last}>
           <Suspense fallback={<>Loading</>}>
